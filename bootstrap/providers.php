250ebaa50124adb3eabf877ede7061f16f3ebdb4
cloudy-2099 <wahyudi.rizky2099@gmail.com>
2026-06-19 20:19:54 +0700

    Force HTTPS in AppServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Illuminate\View\ViewServiceProvider::class,
+    Illuminate\Session\SessionServiceProvider::class,
+    Illuminate\Cache\CacheServiceProvider::class,
 ];
